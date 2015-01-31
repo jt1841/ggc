de610c15e60c5e5cb3ec9a27a8a102f1ad89af8f
Jonathan Talbott <jt1841@gmail.com>
2015-01-31 01:21:27 -0500

    Initial flex Sensor Testing
    This version of the code will read in the raw voltage value from analog
    input 0. This value ranges from 0 -1023. We'll need to run this code to
    determine the min and max values of this voltage at 0 and 90 degrees of
    flex.

---
+++ ggc.ino
@@ -0,0 +1,24 @@
+// ggc.ino
+// Gesture Glove Controller
+// ECE 4012 - Spring 2015
+// Jonathan Talbott, Dan Russell, Justin Denard, Brian Nemsick, Erin Hanson
+
+void setup()
+{
+  Serial.begin(9600);	// Initialize Serial Communication and set rate
+						// Rates: 300, 600, 1200, 2400, 4800, 9600, 14400, 19200, 28800, 38400, 57600, or 115200 bits/second
+}
+
+void loop()
+{
+  int sensor, degrees;
+  
+  sensor = analogRead(0);	// Read voltage from analog input 0
+  degrees = map(sensor, 768, 853, 0, 90);	// Convert reading to degrees. Min and max values will be unique to each flex sensor
+
+  Serial.print("analog input: "); // Print results for debugging
+  Serial.print(sensor,DEC);
+  Serial.print("   degrees: ");
+  Serial.println(degrees,DEC);
+  delay(10);	// Delay in ms before next reading                    
+}
